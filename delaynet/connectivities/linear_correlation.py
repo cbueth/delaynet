7dcb1157d04eab56188f21291220247aef69f45a
Carlson Büth <commit@cbueth.de>
2024-02-09 15:09:40 +0100

    Linear correlation: added option to return absolute value

--- delaynet/connectivities/linear_correlation.py
+++ delaynet/connectivities/linear_correlation.py
@@ -1,13 +1,15 @@
 """Linear correlation (LC) connectivity metric."""
 
-from numpy import argmax
+from numpy import argmax, abs as np_abs
 from scipy.stats import pearsonr
 
 from .connectivity import connectivity
 
 
 @connectivity
-def linear_correlation(ts1, ts2, max_lag_steps: int = 0, **pr_kwargs):
+def linear_correlation(
+    ts1, ts2, max_lag_steps: int = 0, return_abs: bool = True, **pr_kwargs
+):
     """
     Linear correlation (LC) connectivity metric.
 
@@ -17,6 +19,8 @@ def linear_correlation(ts1, ts2, max_lag_steps: int = 0, **pr_kwargs):
     :type ts2: ndarray
     :param max_lag_steps: Maximum time lag to consider.
     :type max_lag_steps: int
+    :param return_abs: Return absolute value of correlation.
+    :type return_abs: bool
     :param pr_kwargs: Keyword arguments forwarded to :func:`scipy.stats.pearsonr`.
     :return: Mutual information value and time lag.
     :rtype: tuple[float, int]
@@ -25,5 +29,5 @@ def linear_correlation(ts1, ts2, max_lag_steps: int = 0, **pr_kwargs):
         pearsonr(ts1[: -k or None], ts2[k:], **pr_kwargs)[0]
         for k in range(max_lag_steps + 1)
     ]
-    idx_max = argmax(all_c)  # TODO: or -argmin? or argmax(abs(all_c))?
-    return all_c[idx_max], idx_max
+    idx_max = argmax(np_abs(all_c))
+    return np_abs(all_c[idx_max]) if return_abs else all_c[idx_max], idx_max

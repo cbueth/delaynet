6827464f0923c518f083934ace4b9a2155b9b2e6
Carlson Büth <commit@cbueth.de>
2025-08-13 17:03:38 +0200

    Bump to `0.3.0`
    Signed-off-by: Carlson Büth <commit@cbueth.de>

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ from datetime import datetime
 project = "delaynet"
 copyright = f"2024–{datetime.now().year}, infomeasure maintainers"
 author = "Carlson Büth"
-version = "0.2.0"
+version = "0.3.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
